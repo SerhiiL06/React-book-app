0c9b19d2aba03f65762ff2a593e6cf575dedd5c7
SerhiiL06 <142268536+SerhiiL06@users.noreply.github.com>
2024-01-08 20:30:14 +0200

    init redux and book functionality

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,17 +1,22 @@
 import { useState } from "react"
 import "./BookForm.css"
-
+import { useDispatch } from "react-redux"
+import { addBook } from "../../redux/books/actionCreators"
 
 const BookForm = () => {
 
     const [title, setTitle] = useState('')
     const [author, setAuthor] = useState('')
 
-    const addBook = (event) => {
+    const dispatch = useDispatch()
+
+    const bookHundler = (event) => {
         event.preventDefault()
+
+        
         if (title && author) {
-            //dispatch
-            console.log(title, author)
+            const book = {title, author}
+            dispatch(addBook(book))
             setTitle('')
             setAuthor('')
         }
@@ -22,7 +27,7 @@ const BookForm = () => {
  
         <div className="app-block book-form">
             <h2>Book Form</h2>
-            <form onSubmit={addBook}>
+            <form onSubmit={bookHundler}>
                 <div>
                     <label htmlFor="title">Title: </label>
                      <input id="title" value={title} type="text" onChange={(el) => setTitle(el.target.value)} />
--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,7 +1,21 @@
+import { useSelector } from "react-redux"
+import "./BookList.css"
+
 const BookList = () => {
+    const state = useSelector((state) => state.books)
+
     return (
         <div className="app-block book-list">
             <h2>Book List</h2>
+            {state.length === 0 ? <p>Not books yet</p> : (
+                <ul>
+                    {state.map((el, i) => {
+                        return <li key={i}>
+                            <div className="book-info">{el.title} by {el.author}</div>
+                        </li>
+                    })}
+                </ul>
+            )}
         </div>
     )
 }
