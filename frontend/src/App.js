833ec6d80a1ae0969a6f8014da331e46a190265f
SerhiiL06 <142268536+SerhiiL06@users.noreply.github.com>
2024-01-08 17:52:24 +0200

    init commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="app-header">
+             <h1>Hello from react</h1>
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
